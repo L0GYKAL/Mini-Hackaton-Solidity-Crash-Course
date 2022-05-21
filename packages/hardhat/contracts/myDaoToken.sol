cc307a8c78a9d21c032d0c7f94410a5e0a5de194
Antoine Mouran <antoine.mouran@epfl.ch>
2022-05-22 01:15:33 +0200

    Second commit

--- packages/hardhat/contracts/myDaoToken.sol
+++ packages/hardhat/contracts/myDaoToken.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/...
 
 
-contract MyDaoToken is ERC20("name", "symbol"){
+contract MyDaoToken is ERC20{
     // At deployement the token should mint its deployer 10'000 tokens
+    // and use the inhereted constructor 'constructor (your agrs) ERC20(name_, symbol_){}'
 
 
 }
